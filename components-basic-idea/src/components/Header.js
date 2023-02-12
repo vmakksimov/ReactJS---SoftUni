1a20e614203b504c0f8f888494d1dfe5029ad1a3
Viktor <vmakksimov@gmail.com>
2023-02-13 01:41:50 +0200

    Components Basic Idea

---
+++ components-basic-idea/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = (props) => {
+    //const reactElement = React.createElement('h1', {}, 'Hello, Viktorio!')
+    const reactElement = <h1>Hello, Viktoriannn!</h1>
+
+    return reactElement
+}
+
+export default Header;
