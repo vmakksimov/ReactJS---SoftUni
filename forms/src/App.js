bbe9fe29fba5bb6e29d666967c4f3a38daad275a
Viktor <vmakksimov@gmail.com>
2023-02-25 03:54:49 +0200

    Forms

---
+++ forms/src/App.js
@@ -0,0 +1,70 @@
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+    const [values, setValues] = useState({
+        username: '',
+        password: '',
+        age: '',
+        bio: '',
+        gender: 'f',
+        userType: 'corporate',
+        tac: false,
+
+    })
+
+    const changeHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name] : e.target.value
+        }))
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+    }
+
+    const checkboxHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name] : e.target.checked
+        }))
+    }
+
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <form onSubmit={submitHandler}>
+                    <div>
+                        <label htmlFor='username'>Username:</label>
+                        <input id='username' type='text' name='username' value={values.username} onChange={changeHandler}/>
+                        
+                    </div>
+                    <div>
+                        <label htmlFor='username'>Password:</label>
+                        <input id='password' type='password' name='password' value={values.password} onChange={changeHandler}/>
+                    </div>
+                    <div>
+                    <label htmlFor='individual-user-type'>Individual:</label>
+                        <input id='pindividual-user-type' type='radio' name='userType' value='individual' onChange={changeHandler}/>
+                    </div>
+                    <div>
+                    <label htmlFor='tac'>Terms and Cond:</label>
+                        <input id='tac' type='checkbox' name='tac' checked={values.tac} onChange={checkboxHandler}/>
+                    </div>
+                    <div>
+                        <input type='submit' value='Register' disabled={!values.tac}/>
+                    </div>
+                    
+                </form>
+            </header>
+        </div>
+    );
+}
+
+export default App;
